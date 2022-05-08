fce8d614c20fffeca05dafcc4ad55c9f590ee5b7
nicokondek <102887440+nicokondek@users.noreply.github.com>
2022-05-08 11:59:12 -0300

    upload basic binary tree
    still have to add insert, delete, level order, balance and rebalance methods.

---
+++ binary_tree.rb
@@ -0,0 +1,47 @@
+class Binarytree
+    attr_accessor :root
+    def initialize(node)
+        self.root = node
+    end
+    
+    def find(value)
+        return if root.value == value
+        
+        return find_helper(root, value)
+    end
+
+    def find_helper(node, value)
+        return if node.nil?
+
+        return true if node.value == value
+
+        found = false
+
+        found = find_helper(node.left, value)
+        return found if found == true
+        return find_helper(node.right, value)
+    end
+
+end
+
+
+class Node
+    attr_accessor :left, :right, :value
+    
+    def initialize (left=nil, right=nil, value=nil)
+        self.left = left
+        self.right = right
+        self.value = value
+    end
+
+end
+
+root = Node.new(nil, nil, 1)
+child = Node.new(nil, nil, 2)
+root.left = child
+bt = Binarytree.new(root)
+puts bt.root.value
+puts bt.root.left.value
+puts bt.root.right
+
+puts bt.find(2)
